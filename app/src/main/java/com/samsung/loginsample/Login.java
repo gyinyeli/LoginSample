995acbf2bbf94cf367903db28543ed750c811390
gyinyeli <gyinyeli@hotmail.com>
2015-08-03 15:53:58 -0700

    test

--- app/src/main/java/com/samsung/loginsample/Login.java
+++ app/src/main/java/com/samsung/loginsample/Login.java
@@ -1,7 +1,12 @@
 package com.samsung.loginsample;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,11 +23,30 @@ public class Login extends ActionBarActivity {
     private static Button  login_btn;
     int attemp_counter=5;
 
+    private static final String LOGTAG="Login";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setLogo(R.mipmap.ic_launcher);
+
+        //actionBar().setLogo(R.mipmap.ic_launcher);
+
         setContentView(R.layout.activity_login);
+
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        getSupportActionBar().setLogo(R.drawable.ic_launcher);
+//        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+//        SetApplicationAttribute(android:icon, "@mipmap/ic_launcher");
         LoginButton();
+
+        Toast msg = Toast.makeText(this, "Login create", Toast.LENGTH_LONG);
+        msg.show();
+
+        Log.d(LOGTAG, "onCreate");
     }
 
     @Override
@@ -46,7 +70,10 @@ public class Login extends ActionBarActivity {
                     public void onClick(View v) {
                         if (username.getText().toString().equals("gang.yin") && password.getText().toString().equals("gang.yin")) {
                             Toast.makeText(Login.this, "user and password is correct", Toast.LENGTH_SHORT).show();
-                            android.content.Intent intent = new android.content.Intent("com.samsung.loginsample.User");
+
+                            //android.content.Intent intent = new android.content.Intent("com.samsung.loginsample.User");
+                            Intent intent = new Intent(Login.this, User.class);
+
                             startActivity(intent);
                         } else {
                             Toast.makeText(Login.this, "user and password is not correct", Toast.LENGTH_SHORT).show();
@@ -67,12 +94,83 @@ public class Login extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        Toast.makeText(this, "You selected " + item.getTitle(), Toast.LENGTH_LONG).show();
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
+        Intent intent=null;
+        switch(id) {
+            case R.id.action_activity_two:
+                intent = new Intent(Login.this, User.class);
+                startActivity(intent);
+                break;
+            case R.id.action_external_url:
+                String webpage="http://developer.android.com/index.html";
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse(webpage));
+                startActivity(intent);
+                break;
+            case R.id.action_send_message:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, "Hello from gang yin for the test");
+                intent.setType("text/plain");
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+
+
         return super.onOptionsItemSelected(item);
     }
+
+    public void gotoActivity(View v){
+        Intent intent = new Intent(this, User.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOGTAG, "onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOGTAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOGTAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOGTAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOGTAG, "onDestroy");
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if(newConfig.orientation==Configuration.ORIENTATION_LANDSCAPE){
+            Toast.makeText(this, "Landscape!", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(this, "Portrait!", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
 }
--- app/src/main/java/com/samsung/loginsample/User.java
+++ app/src/main/java/com/samsung/loginsample/User.java
@@ -1,9 +1,13 @@
 package com.samsung.loginsample;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class User extends ActionBarActivity {
@@ -11,14 +15,31 @@ public class User extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_user);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_user, menu);
-        return true;
+        //getMenuInflater().inflate(R.menu.menu_user, menu);
+        //return true;
+        MenuItem item = menu.add(R.string.external_url);
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                String webpage="http://developer.android.com/index.html";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(webpage));
+                startActivity(intent);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -33,6 +54,24 @@ public class User extends ActionBarActivity {
             return true;
         }
 
+        //if(id==R.id.home){
+            //finish();
+        //}
+
         return super.onOptionsItemSelected(item);
+    }
+
+    public void implicitViewURL(View v){
+        String webpage="http://developer.android.com/index.html";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(webpage));
+        startActivity(intent);
+    }
+
+    public void implicitSendText(View v){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, "Hello from gang yin for the test");
+        intent.setType("text/plain");
+        startActivity(intent);
     }
 }
